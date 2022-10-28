3a0d673912770c15fd467f2fc00ba21836144da6
Apollo-111 <110030401+Apollo-111@users.noreply.github.com>
2022-10-28 12:17:13 +0000

    Deploying to public from @ Apollo-111/deploy-next@835787bfd4d44425e9bfcd64a66668605769813d 🚀

---
+++ _next/static/e4XHu9MyATrr9Vv06QXyo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/57-2db1494f0ed46dc7.js","static/chunks/pages/index-3b1164b1412ac7c7.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
